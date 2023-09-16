2d02ffcfdeca9cfda7d121493570d0b5024908c7
Rafael Acquaviva <rafaacqv@gmail.com>
2023-09-16 20:43:11 -0300

    Remove unused imports

--- PUC.PosGraduacao.BookStore.Domain/Models/Products.cs
+++ PUC.PosGraduacao.BookStore.Domain/Models/Products.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PUC.PosGraduacao.BookStore.Domain.Models
+﻿namespace PUC.PosGraduacao.BookStore.Domain.Models
 {
   public class Products
   {
