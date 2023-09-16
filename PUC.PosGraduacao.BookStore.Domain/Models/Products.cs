076eb2a4a943b77ffdccbcaf3cc172a894cdd8a4
Rafael Acquaviva <rafaacqv@gmail.com>
2023-09-16 20:42:32 -0300

    Create first domain class

---
+++ PUC.PosGraduacao.BookStore.Domain/Models/Products.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PUC.PosGraduacao.BookStore.Domain.Models
+{
+  public class Products
+  {
+    public int Id { get; set; }
+    public string? Title { get; set; }
+  }
+}
