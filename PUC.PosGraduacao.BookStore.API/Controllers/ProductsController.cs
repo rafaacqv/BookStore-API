49276d491356919ccabd493bc97e885ca2722325
Rafael Acquaviva <rafaacqv@gmail.com>
2023-09-16 20:37:13 -0300

    Create first controller and project domain

---
+++ PUC.PosGraduacao.BookStore.API/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PUC.PosGraduacao.BookStore.API.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class ProductsController : ControllerBase
+  {
+    [HttpGet]
+    public string GetProducts()
+    {
+      return "Products List";
+    }
+
+    [HttpGet("{id}")]
+    public string GetProduct(int id)
+    {
+      return "Product";
+    }
+  }
+}
