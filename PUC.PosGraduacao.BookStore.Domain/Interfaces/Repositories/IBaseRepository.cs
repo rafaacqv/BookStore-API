1c76cfd33d69fb2ab5c98c98013684fa6c3db1bd
Rafael Acquaviva <rafaacqv@gmail.com>
2023-09-17 00:55:17 -0300

    Creating generic repository

---
+++ PUC.PosGraduacao.BookStore.Domain/Interfaces/Repositories/IBaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Principal;
+
+namespace PUC.PosGraduacao.BookStore.Domain.Interfaces.Repositories
+{
+  public interface IBaseRepository<T> where T : class, IEntity
+  {
+    IQueryable<T> GetAll();
+    Task<T> GetById(int id);
+    Task Create(T entity);
+    Task Update(int id, T entity);
+    Task Delete(int id);
+  }
+}
---
+++ PUC.PosGraduacao.BookStore.Infra.Data/Repositories/BaseRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PUC.PosGraduacao.BookStore.Domain.Interfaces;
+using PUC.PosGraduacao.BookStore.Domain.Interfaces.Repositories;
+using PUC.PosGraduacao.BookStore.Infra.Data.Contexts;
+
+namespace PUC.PosGraduacao.BookStore.Infra.Data.Repositories
+{
+  public class BaseRepository<T> : IBaseRepository<T> where T: class, IEntity
+  {
+    private readonly ApplicationDbContext _context;
+    public BaseRepository(ApplicationDbContext context)
+    {
+      _context = context;        
+    }
+
+    public async Task Create(T entity)
+    {
+      await _context.Set<T>().AddAsync(entity);
+      await _context.SaveChangesAsync();
+    }
+
+    public async Task Delete(int id)
+    {
+      var entity = await GetById(id);
+      _context.Set<T>().Remove(entity);
+      await _context.SaveChangesAsync();
+    }
+
+    public IQueryable<T> GetAll()
+    {
+      return _context.Set<T>().AsNoTracking();
+    }
+
+    public async Task<T> GetById(int id)
+    {
+      var item =  await _context.Set<T>()
+                 .AsNoTracking()
+                 .FirstOrDefaultAsync(e => e.Id == id);
+
+      _ = item ?? throw new ArgumentNullException(nameof(item));
+
+      return item;
+
+    }
+
+    public async Task Update(int id, T entity)
+    {
+      _context.Set<T>().Update(entity);
+      await _context.SaveChangesAsync();
+    }
+  }
+}
