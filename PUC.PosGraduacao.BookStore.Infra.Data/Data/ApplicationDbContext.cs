bec7378e82fe4acec3f423e849cdef6e016ae029
Rafael Acquaviva <rafaacqv@gmail.com>
2023-09-16 22:10:56 -0300

    Create data configuration project

---
+++ PUC.PosGraduacao.BookStore.Infra.Data/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PUC.PosGraduacao.BookStore.Domain.Models;
+
+namespace PUC.PosGraduacao.BookStore.Infra.Data.Data
+{
+  public class ApplicationDbContext : DbContext
+  {
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Products> Products { get; set; }
+  }
+}
