8139be6becc1a132e153c146016352b285d08319
Rafael Acquaviva <rafaacqv@gmail.com>
2023-09-18 23:01:35 -0300

    Add specification pattern to domain

---
+++ PUC.PosGraduacao.BookStore.Domain/Specifications/BaseSpecification.cs
@@ -0,0 +1,22 @@
+﻿using PUC.PosGraduacao.BookStore.Domain.Interfaces.Specifications;
+using System.Linq.Expressions;
+
+namespace PUC.PosGraduacao.BookStore.Domain.Specifications
+{
+  public class BaseSpecification<T> : ISpecification<T>
+  {
+    public BaseSpecification(Expression<Func<T, bool>> criteria)
+    {
+      Criteria = criteria;
+    }
+
+    public Expression<Func<T, bool>> Criteria { get; }
+    public List<Expression<Func<T, object>>> Includes { get; } = 
+      new List<Expression<Func<T, object>>>();
+
+    protected void AddInclude(Expression<Func<T, object>> includeExpression)
+    {
+      Includes.Add(includeExpression);
+    }
+  }
+}
