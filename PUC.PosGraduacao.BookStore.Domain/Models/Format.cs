e24d772501fda634e59a543394fcb3a9eb00eeb2
Rafael Acquaviva <rafaacqv@gmail.com>
2023-09-20 00:18:22 -0300

    Clean usings

--- PUC.PosGraduacao.BookStore.Domain/Models/Format.cs
+++ PUC.PosGraduacao.BookStore.Domain/Models/Format.cs
@@ -1,11 +1,4 @@
-﻿using PUC.PosGraduacao.BookStore.Domain.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PUC.PosGraduacao.BookStore.Domain.Models
+﻿namespace PUC.PosGraduacao.BookStore.Domain.Models
 {
   public class Format : BaseEntity
   {
