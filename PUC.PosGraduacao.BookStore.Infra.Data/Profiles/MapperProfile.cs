3bfc0db8ac2b360f392feba8ea89dfa956d3a239
Rafael Acquaviva <rafaacqv@gmail.com>
2023-09-20 22:03:48 -0300

    Configure value resolver auto mapper

--- PUC.PosGraduacao.BookStore.Infra.Data/Profiles/MapperProfile.cs
+++ PUC.PosGraduacao.BookStore.Infra.Data/Profiles/MapperProfile.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using PUC.PosGraduacao.BookStore.Domain.DTO;
 using PUC.PosGraduacao.BookStore.Domain.Models;
+using PUC.PosGraduacao.BookStore.Infra.Data.ValueResolvers;
 
 namespace PUC.PosGraduacao.BookStore.Infra.Data.Profiles
 {
@@ -11,12 +12,14 @@ namespace PUC.PosGraduacao.BookStore.Infra.Data.Profiles
       CreateMap<Category, CategoryResponse>();
       CreateMap<Format, FormatResponse>();
       CreateMap<Product, ProductsResponse>();
-      
+
       CreateMap<Product, ProductDTO>()
         .ForMember(dest => dest.Category,
         opt => opt.MapFrom(src => src.Category.Name))
         .ForMember(dest => dest.Format,
-        opt => opt.MapFrom(src => src.Format.Type));
+        opt => opt.MapFrom(src => src.Format.Type))
+        .ForMember(dest => dest.ImageUrl,
+        opt => opt.MapFrom<UrlValueResolver>());
     }
   }
 }
---
+++ PUC.PosGraduacao.BookStore.Infra.Data/ValueResolvers/UrlValueResolver.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Configuration;
+using PUC.PosGraduacao.BookStore.Domain.DTO;
+using PUC.PosGraduacao.BookStore.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PUC.PosGraduacao.BookStore.Infra.Data.ValueResolvers
+{
+    public class UrlValueResolver : IValueResolver<Product, ProductDTO, string>
+    {
+        private readonly IConfiguration _configuration;
+
+        public UrlValueResolver(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public string Resolve(Product source, ProductDTO destination, string destMember, ResolutionContext context)
+        {
+            if (!string.IsNullOrEmpty(source.ImageUrl))
+            {
+                return _configuration["ApiUrl"] + source.ImageUrl;
+            }
+
+            return null;
+        }
+    }
+}
