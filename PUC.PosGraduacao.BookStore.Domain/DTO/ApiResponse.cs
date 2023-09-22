ce97e4dc433f7f07352260aeaaaffff4eaa2aab9
Rafael Acquaviva <rafaacqv@gmail.com>
2023-09-22 01:32:02 -0300

    Create new API response class

---
+++ PUC.PosGraduacao.BookStore.Domain/DTO/ApiResponse.cs
@@ -0,0 +1,26 @@
+﻿namespace PUC.PosGraduacao.BookStore.Domain.DTO
+{
+  public class ApiResponse
+  {
+    public ApiResponse(int statusCode, string message = null) 
+    {
+      StatusCode = statusCode;
+      Message = message ?? GetDefaultMessageForStatusCode(statusCode);
+    }
+
+    public int StatusCode { get; set; }
+    public string? Message { get; set; }
+
+    private string? GetDefaultMessageForStatusCode(int statusCode)
+    {
+      return statusCode switch
+      {
+        400 => "Bad Request",
+        401 => "Unauthorized",
+        404 => "Resource not found",
+        500 => "Internal Server Error",
+        _ => null
+      };
+    }
+  }
+}
