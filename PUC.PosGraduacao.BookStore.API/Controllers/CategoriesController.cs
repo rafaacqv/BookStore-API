62d4d218b1ea1c5627540faf1e6b603aa9e93022
Rafael Acquaviva <rafaacqv@gmail.com>
2023-09-22 00:40:15 -0300

    Remove usings

--- PUC.PosGraduacao.BookStore.API/Controllers/CategoriesController.cs
+++ PUC.PosGraduacao.BookStore.API/Controllers/CategoriesController.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PUC.PosGraduacao.BookStore.Domain.DTO;
 using PUC.PosGraduacao.BookStore.Domain.Interfaces.Services;
-using PUC.PosGraduacao.BookStore.Domain.Models;
 using PUC.PosGraduacao.BookStore.Domain.Enums;
 
 namespace PUC.PosGraduacao.BookStore.API.Controllers
