2ec34707376894034c8cd236a18604ba821c451f
Rafael Acquaviva <rafaacqv@gmail.com>
2023-09-23 18:57:59 -0300

    Remove usings

--- PUC.PosGraduacao.BookStore.Infra.Data/Config/EFConfig/CategoryConfiguration.cs
+++ PUC.PosGraduacao.BookStore.Infra.Data/Config/EFConfig/CategoryConfiguration.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using PUC.PosGraduacao.BookStore.Domain.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PUC.PosGraduacao.BookStore.Infra.Data.Config.EFConfig
 {
--- PUC.PosGraduacao.BookStore.Infra.Data/ValueResolvers/UrlValueResolver.cs
+++ PUC.PosGraduacao.BookStore.Infra.Data/ValueResolvers/UrlValueResolver.cs
@@ -2,11 +2,6 @@
 using Microsoft.Extensions.Configuration;
 using PUC.PosGraduacao.BookStore.Domain.DTO;
 using PUC.PosGraduacao.BookStore.Domain.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PUC.PosGraduacao.BookStore.Infra.Data.ValueResolvers
 {
