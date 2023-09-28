cecc3a7c287806d77eece1fc42232f4f03233edd
Rafael Acquaviva <rafaacqv@gmail.com>
2023-09-27 22:51:31 -0300

    Add book image to static files

--- PUC.PosGraduacao.BookStore.Domain/DTO/CategoriesListResponse.cs
+++ PUC.PosGraduacao.BookStore.Domain/DTO/CategoriesListResponse.cs
@@ -1,9 +1,4 @@
 ﻿using PUC.PosGraduacao.BookStore.Domain.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PUC.PosGraduacao.BookStore.Domain.DTO
 {
