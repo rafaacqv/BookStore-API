6795c6d92a250a42c3f0377d332b6025bb04bdcf
Rafael Acquaviva <rafaacqv@gmail.com>
2023-09-30 18:33:17 -0300

    Remove unused imports

---
+++ PUC.PosGraduacao.BookStore.Domain/Interfaces/Repositories/IBasketRepository.cs
@@ -0,0 +1,11 @@
+﻿using PUC.PosGraduacao.BookStore.Domain.Models;
+
+namespace PUC.PosGraduacao.BookStore.Domain.Interfaces.Repositories
+{
+  public interface IBasketRepository
+  {
+    Task<CustomerBasket> GetBasketAsync(string basketId);
+    Task<CustomerBasket> UpdateBasketAsync(CustomerBasket basket);
+    Task<bool> DeleteBasketAsync(string basketId);
+  }
+}
