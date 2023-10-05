e6f2866271f2c7570b9e77384f251bd56818885e
Rafael Acquaviva <rafaacqv@gmail.com>
2023-10-04 21:12:29 -0300

    Create Identity abstractions

---
+++ PUC.PosGraduacao.BookStore.Domain/Models/Identity/AppUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Net.Sockets;
+
+namespace PUC.PosGraduacao.BookStore.Domain.Models.Identity
+{
+  public class AppUser : IdentityUser
+  {
+    public string DisplayName { get; set; }
+    public Address Address { get; set; }
+  }
+}
