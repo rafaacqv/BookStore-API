f9a6fa80d3c6df9d98ab5c504877dce147defa0a
Rafael Acquaviva <rafaacqv@gmail.com>
2023-10-05 00:04:11 -0300

    Create login method

---
+++ PUC.PosGraduacao.BookStore.API/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PUC.PosGraduacao.BookStore.Domain.DTO;
+using PUC.PosGraduacao.BookStore.Domain.Models.Identity;
+
+namespace PUC.PosGraduacao.BookStore.API.Controllers
+{
+  public class AccountController : BaseApiController
+  {
+    private readonly UserManager<AppUser> _userManager;
+    private readonly SignInManager<AppUser> _signInManager;
+
+    public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+    {
+      _signInManager = signInManager;
+      _userManager = userManager;
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<UserDTO>> Login(LoginDTO loginDTO)
+    {
+      var user = await _userManager.FindByEmailAsync(loginDTO.Email);
+      if (user == null) return Unauthorized(new ApiResponse(401));
+
+      var result = await _signInManager.CheckPasswordSignInAsync(user, loginDTO.Password, false);
+      if (!result.Succeeded) return Unauthorized(new ApiResponse(401));
+
+      return new UserDTO
+      {
+        Email = user.Email,
+        Token = "Token Placeholder",
+        DisplayName = user.DisplayName,
+      };
+    }
+  }
+}
