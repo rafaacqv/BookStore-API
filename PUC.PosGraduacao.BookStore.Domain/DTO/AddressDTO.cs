7bce1ab1b76cd3cf26c1acb4175542701ae3bda6
Rafael Acquaviva <rafaacqv@gmail.com>
2023-10-06 23:23:13 -0300

    Configure address data anotations

--- PUC.PosGraduacao.BookStore.Domain/DTO/AddressDTO.cs
+++ PUC.PosGraduacao.BookStore.Domain/DTO/AddressDTO.cs
@@ -1,18 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+
 
 namespace PUC.PosGraduacao.BookStore.Domain.DTO
 {
   public class AddressDTO
   {
+    [Required]
     public string FirstName { get; set; }
+    [Required]
     public string LastName { get; set; }
+    [Required]
     public string Street { get; set; }
+    [Required]
     public string City { get; set; }
+    [Required]
     public string State { get; set; }
+    [Required]
     public string ZipCode { get; set; }
   }
 }
