ce96c733b7e680a520208d516a37c8e63f26bab4
Rafael Acquaviva <rafaacqv@gmail.com>
2023-10-08 17:26:52 -0300

    Implemented entities configuration

---
+++ PUC.PosGraduacao.BookStore.Infra.Data/Config/EFConfig/DeliveryMethodConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PUC.PosGraduacao.BookStore.Domain.Models.Order;
+
+namespace PUC.PosGraduacao.BookStore.Infra.Data.Config.EFConfig
+{
+  public class DeliveryMethodConfiguration : IEntityTypeConfiguration<DeliveryMethod>
+  {
+    public void Configure(EntityTypeBuilder<DeliveryMethod> builder)
+    {
+      builder.Property(d => d.Price)
+        .HasColumnType("decimal{18,2}");
+    }
+  }
+}
---
+++ PUC.PosGraduacao.BookStore.Infra.Data/Config/EFConfig/OrderItemConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PUC.PosGraduacao.BookStore.Domain.Models.Order;
+
+namespace PUC.PosGraduacao.BookStore.Infra.Data.Config.EFConfig
+{
+  public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
+  {
+    public void Configure(EntityTypeBuilder<OrderItem> builder)
+    {
+      builder.OwnsOne(i => i.ItemOrdered, io => { io.WithOwner(); });
+      builder.Property(i => i.Price)
+        .HasColumnType("decimal{18,2}");
+    }
+  }
+}
