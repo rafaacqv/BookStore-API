2551370c4da3e95b93d784f793b914dd0a669539
Rafael Acquaviva <rafaacqv@gmail.com>
2023-10-14 16:58:30 -0300

    Release commit

--- PUC.PosGraduacao.BookStore.Infra.Data/Contexts/ApplicationDbContextSeed.cs
+++ PUC.PosGraduacao.BookStore.Infra.Data/Contexts/ApplicationDbContextSeed.cs
@@ -1,5 +1,6 @@
 ﻿using PUC.PosGraduacao.BookStore.Domain.Models;
 using PUC.PosGraduacao.BookStore.Domain.Models.Order;
+using System.Reflection;
 using System.Text.Json;
 
 namespace PUC.PosGraduacao.BookStore.Infra.Data.Contexts
@@ -8,9 +9,11 @@ namespace PUC.PosGraduacao.BookStore.Infra.Data.Contexts
   {
     public static async Task SeedAsync(ApplicationDbContext context)
     {
+      var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
       if (!context.Formats.Any())
       {
-        var formatsData = File.ReadAllText("../PUC.PosGraduacao.BookStore.Infra.Data/SeedData/formats.json");
+        var formatsData = File.ReadAllText(path + @"/SeedData/formats.json");
         var formats = JsonSerializer.Deserialize<List<Format>>(formatsData);
 
         _ = formats ?? throw new ArgumentNullException(nameof(formats));
@@ -19,7 +22,7 @@ namespace PUC.PosGraduacao.BookStore.Infra.Data.Contexts
 
       if (!context.Categories.Any())
       {
-        var categoriesData = File.ReadAllText("../PUC.PosGraduacao.BookStore.Infra.Data/SeedData/categories.json");
+        var categoriesData = File.ReadAllText(path + @"/SeedData/categories.json");
         var categories = JsonSerializer.Deserialize<List<Category>>(categoriesData);
 
         _ = categories ?? throw new ArgumentNullException(nameof(categories));
@@ -28,7 +31,7 @@ namespace PUC.PosGraduacao.BookStore.Infra.Data.Contexts
 
       if (!context.Products.Any())
       {
-        var productsData = File.ReadAllText("../PUC.PosGraduacao.BookStore.Infra.Data/SeedData/products.json");
+        var productsData = File.ReadAllText(path + @"/SeedData/products.json");
         var products = JsonSerializer.Deserialize<List<Product>>(productsData);
 
         _ = products ?? throw new ArgumentNullException(nameof(products));
@@ -37,7 +40,7 @@ namespace PUC.PosGraduacao.BookStore.Infra.Data.Contexts
 
       if (!context.DeliveryMethods.Any())
       {
-        var deliveryData = File.ReadAllText("../PUC.PosGraduacao.BookStore.Infra.Data/SeedData/delivery.json");
+        var deliveryData = File.ReadAllText(path + @"/SeedData/delivery.json");
         var methods = JsonSerializer.Deserialize<List<DeliveryMethod>>(deliveryData);
 
         _ = methods ?? throw new ArgumentNullException(nameof(methods));
